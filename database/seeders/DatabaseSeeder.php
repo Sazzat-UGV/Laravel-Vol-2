1109d457eccb082b54aba62f812da451343ad17f
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2023-06-12 18:39:50 +0600

    Module seeder and permission seeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            UserSeeder::class,
+            ModuleSeeder::class,
+            PermissionSeeder::class,
+            // UserSeeder::class,
         ]);
     }
 }
